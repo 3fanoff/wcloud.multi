0579f070e8c4704a8e27ec584c057b55f22f886a
3fanoff <3fanoff@mail.ru>
2026-09-24 19:20:00 +0300

    WCL-OTP-1: fixed mobile viewport sizing

--- src/main.js
+++ src/main.js
@@ -1,20 +1,47 @@
 import './scss/main.scss';
 import Tab from "bootstrap/js/src/tab";
 
+const isDebug = Boolean(localStorage.getItem('debug') || location.search.indexOf('debug') >= 0);
+let debugNode = null;
+
 function setDimensions () {
     document.documentElement.style.overflowY = 'scroll';
     const baseWidth = 1920;
-    const dimension = ((document.documentElement.clientWidth / baseWidth) * window.devicePixelRatio).toFixed(3);
+    const ratio = document.documentElement.clientWidth < 980 ? 1.2 : window.devicePixelRatio;
+    const dimension = ((document.documentElement.clientWidth / baseWidth) * ratio).toFixed(3);
+    fillDebug({
+        'doc.clientWidth' : document.documentElement.clientWidth,
+        'doc.scrollWidth' : document.documentElement.scrollWidth,
+        'window.innerWidth': window.innerWidth,
+        'devicePixelRatio' : window.devicePixelRatio,
+        'ratio': ratio,
+        'dimension': dimension,
+    })
     console.log(document.readyState, document.documentElement.clientWidth, window.devicePixelRatio);
     document.documentElement.style.setProperty('--dimension', String(dimension))
     document.documentElement.style.fontSize = String(dimension) + 'rem';
     document.documentElement.style.overflowY = '';
 }
 
+function fillDebug(data) {
+    if (isDebug) {
+        if (debugNode === null) {
+            debugNode = document.getElementById('debug');
+        }
+        let line = '';
+        for (let key in data) {
+            line += key + ': ' + String(data[key]) + '<br>';
+        }
+        debugNode.innerHTML = line;
+        debugNode.style.display = 'block';
+    }
+}
+
 window.addEventListener('resize', () => {
     setDimensions()
 })
 document.addEventListener('DOMContentLoaded', () => {
+    //setTimeout(() => window.dispatchEvent(new Event('resize')), 0);
     setDimensions();
 
     const tabs = document.querySelectorAll('.js-tab');
